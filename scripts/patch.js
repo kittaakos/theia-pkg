d12ac69a0aa65d4579a582413e69cafc8a28c0e1
Akos Kitta <kittaakos@typefox.io>
2019-04-18 08:48:18 +0000

    Patched the `express` code with the `index.html`.
    Signed-off-by: Akos Kitta <kittaakos@typefox.io>

--- scripts/patch.js
+++ scripts/patch.js
@@ -1,23 +1,31 @@
-// Patches the `node_modules` and `src-gen` code with the changes from https://github.com/theia-ide/theia/pull/4941
 (() => {
     const fs = require('fs');
     const path = require('path');
-    const force = fs.constants.COPYFILE_FICLONE;
+    // Patch the `server`.
+    // See: https://github.com/theia-ide/theia/issues/4944
+    fs.copyFileSync(
+        path.resolve(__dirname, 'GH-4940/backend/server.js'),
+        path.resolve(__dirname, '../src-gen/backend/server.js'),
+        fs.constants.COPYFILE_FICLONE
+    );
+    // Patches the `node_modules` and `src-gen` code with the changes from https://github.com/theia-ide/theia/pull/4941
+    // Patch the backend `main`.
+    fs.copyFileSync(
+        path.resolve(__dirname, 'GH-4940/backend/main.js'),
+        path.resolve(__dirname, '../src-gen/backend/main.js'),
+        fs.constants.COPYFILE_FICLONE
+    );
+    // Patch Theia `main`.
     fs.copyFileSync(
         path.resolve(__dirname, 'GH-4940/theia-main/main.ts'),
         path.resolve(__dirname, '../node_modules/@theia/core/src/node/main.ts'),
-        force
-    );
-    fs.copyFileSync(
-        path.resolve(__dirname, 'GH-4940/app-main/main.js'),
-        path.resolve(__dirname, '../src-gen/backend/main.js'),
-        force
+        fs.constants.COPYFILE_FICLONE
     );
     for (const name of ['main.d.ts', 'main.d.ts.map', 'main.js']) {
         fs.copyFileSync(
             path.resolve(__dirname, `GH-4940/theia-main/${name}`),
             path.resolve(__dirname, `../node_modules/@theia/core/lib/node/${name}`),
-            force
+            fs.constants.COPYFILE_FICLONE
         );
     }
 })();
